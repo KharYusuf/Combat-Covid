0200f1a97799d1a05672a928e06cda87215de505
vivekramnani <vivekramnani@gmail.com>
2020-11-19 13:49:55 +0530

    ScrollView for charts with more data

--- lib/screens/chart.dart
+++ lib/screens/chart.dart
@@ -5,71 +5,216 @@ import 'package:charts_flutter/flutter.dart' as charts;
 
 class Chart extends StatelessWidget {
   final List<ScrapedData> casesList = [
-    ScrapedData(DateTime.now(), 10000),
-    ScrapedData(DateTime.now().add(new Duration(days: 1)), 5000),
-    ScrapedData(DateTime.now().add(new Duration(days: 2)), 7000),
+    ScrapedData(DateTime.now().add(new Duration(days: -6)), 40000),
+    ScrapedData(DateTime.now().add(new Duration(days: -5)), 35000),
+    ScrapedData(DateTime.now().add(new Duration(days: -4)), 37000),
+    ScrapedData(DateTime.now().add(new Duration(days: -3)), 35000),
+    ScrapedData(DateTime.now().add(new Duration(days: -2)), 33000),
+    ScrapedData(DateTime.now().add(new Duration(days: -1)), 30000),
+    ScrapedData(DateTime.now(), 31000),
+  ];
+  final List<ScrapedData> deathsList = [
+    ScrapedData(DateTime.now().add(new Duration(days: -6)), 900),
+    ScrapedData(DateTime.now().add(new Duration(days: -5)), 800),
+    ScrapedData(DateTime.now().add(new Duration(days: -4)), 950),
+    ScrapedData(DateTime.now().add(new Duration(days: -3)), 700),
+    ScrapedData(DateTime.now().add(new Duration(days: -2)), 750),
+    ScrapedData(DateTime.now().add(new Duration(days: -1)), 700),
+    ScrapedData(DateTime.now(), 688),
+  ];
+  final List<ScrapedData> testsList = [
+    ScrapedData(DateTime.now().add(new Duration(days: -6)), 1000000),
+    ScrapedData(DateTime.now().add(new Duration(days: -5)), 1100021),
+    ScrapedData(DateTime.now().add(new Duration(days: -4)), 1100120),
+    ScrapedData(DateTime.now().add(new Duration(days: -3)), 921421),
+    ScrapedData(DateTime.now().add(new Duration(days: -2)), 1200321),
+    ScrapedData(DateTime.now().add(new Duration(days: -1)), 1200300),
+    ScrapedData(DateTime.now(), 1102200),
   ];
   @override
   Widget build(BuildContext context) {
-    List<charts.Series<ScrapedData, String>> series = [
+    List<charts.Series<ScrapedData, String>> cases = [
       charts.Series(
         id: "Cases",
         data: casesList,
         domainFn: (ScrapedData series, _) =>
-            DateFormat('yyyy-MM-dd').format(series.date),
+            DateFormat('dd-MM').format(series.date),
         measureFn: (ScrapedData series, _) => series.cases,
         colorFn: (ScrapedData series, _) =>
             charts.ColorUtil.fromDartColor(Colors.blue),
       )
     ];
+
+    List<charts.Series<ScrapedData, String>> deaths = [
+      charts.Series(
+        id: "Cases",
+        data: deathsList,
+        domainFn: (ScrapedData series, _) =>
+            DateFormat('dd-MM').format(series.date),
+        measureFn: (ScrapedData series, _) => series.cases,
+        colorFn: (ScrapedData series, _) =>
+            charts.ColorUtil.fromDartColor(Colors.blue),
+      )
+    ];
+
+    List<charts.Series<ScrapedData, String>> tests = [
+      charts.Series(
+        id: "Cases",
+        data: testsList,
+        domainFn: (ScrapedData series, _) =>
+            DateFormat('dd-MM').format(series.date),
+        measureFn: (ScrapedData series, _) => series.cases,
+        colorFn: (ScrapedData series, _) =>
+            charts.ColorUtil.fromDartColor(Colors.blue),
+      )
+    ];
+
     return new Scaffold(
       appBar: AppBar(
-        title: Text("Combat Covid"),
+        title: Text("Statistics"),
       ),
-      body: Container(
-        height: 400,
-        padding: EdgeInsets.all(8.0),
-        child: Card(
-          child: Padding(
-            padding: EdgeInsets.all(6.0),
-            child: Column(
-              children: <Widget>[
-                Text("Cases per day"),
-                Expanded(
-                  child: charts.BarChart(
-                    series,
-                    animate: true,
-                    domainAxis: new charts.OrdinalAxisSpec(
-                      renderSpec: new charts.SmallTickRendererSpec(
-                        labelStyle: new charts.TextStyleSpec(
-                          fontSize: 14, // size in Pts.
-                          color:
-                              charts.MaterialPalette.blue.shadeDefault.darker,
-                        ),
-                        lineStyle: new charts.LineStyleSpec(
-                          color:
-                              charts.MaterialPalette.blue.shadeDefault.darker,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 400,
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(6.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text("New Cases per day"),
+                      Expanded(
+                        child: charts.BarChart(
+                          cases,
+                          animate: true,
+                          domainAxis: new charts.OrdinalAxisSpec(
+                            renderSpec: new charts.SmallTickRendererSpec(
+                              labelStyle: new charts.TextStyleSpec(
+                                fontSize: 14,
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                              lineStyle: new charts.LineStyleSpec(
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                            ),
+                          ),
+                          primaryMeasureAxis: new charts.NumericAxisSpec(
+                            renderSpec: new charts.GridlineRendererSpec(
+                              labelStyle: new charts.TextStyleSpec(
+                                fontSize: 14,
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                              lineStyle: new charts.LineStyleSpec(
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                    primaryMeasureAxis: new charts.NumericAxisSpec(
-                      renderSpec: new charts.GridlineRendererSpec(
-                        labelStyle: new charts.TextStyleSpec(
-                          fontSize: 14,
-                          color:
-                              charts.MaterialPalette.blue.shadeDefault.darker,
-                        ),
-                        lineStyle: new charts.LineStyleSpec(
-                          color:
-                              charts.MaterialPalette.blue.shadeDefault.darker,
-                        ),
-                      ),
-                    ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
+            Container(
+              height: 400,
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(6.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text("Deaths per day"),
+                      Expanded(
+                        child: charts.BarChart(
+                          deaths,
+                          animate: true,
+                          domainAxis: new charts.OrdinalAxisSpec(
+                            renderSpec: new charts.SmallTickRendererSpec(
+                              labelStyle: new charts.TextStyleSpec(
+                                fontSize: 14,
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                              lineStyle: new charts.LineStyleSpec(
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                            ),
+                          ),
+                          primaryMeasureAxis: new charts.NumericAxisSpec(
+                            renderSpec: new charts.GridlineRendererSpec(
+                              labelStyle: new charts.TextStyleSpec(
+                                fontSize: 14,
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                              lineStyle: new charts.LineStyleSpec(
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 400,
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(6.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text("Tests Performed"),
+                      Expanded(
+                        child: charts.BarChart(
+                          tests,
+                          animate: true,
+                          domainAxis: new charts.OrdinalAxisSpec(
+                            renderSpec: new charts.SmallTickRendererSpec(
+                              labelStyle: new charts.TextStyleSpec(
+                                fontSize: 14,
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                              lineStyle: new charts.LineStyleSpec(
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                            ),
+                          ),
+                          primaryMeasureAxis: new charts.NumericAxisSpec(
+                            renderSpec: new charts.GridlineRendererSpec(
+                              labelStyle: new charts.TextStyleSpec(
+                                fontSize: 14,
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                              lineStyle: new charts.LineStyleSpec(
+                                color: charts
+                                    .MaterialPalette.blue.shadeDefault.darker,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
